e5e89089dbc86c967f294b3e573edf328bf7999f
Palmer-biofi <97751301+Palmer-biofi@users.noreply.github.com>
2024-01-20 22:21:27 -0700

    update normalizeURL to use URL class

--- crawl.js
+++ crawl.js
@@ -1,11 +1,10 @@
 function normalizeURL(url) {
-    url = url.split('://')[1]
-    url = url.split('?')[0]
-    if (url.at(-1) === '/') {
-        url = url.slice(0, -1)
-    }
-    return url
+    const urlObject = new URL(url)
+    const pathname = urlObject.pathname.at(-1) === '/' ? urlObject.pathname : `${urlObject.pathname}/`
+    return `${urlObject.hostname}/${pathname}`
 }
+
+const url = new URL('http://example.com/path/')
 
 module.exports = {
     normalizeURL
