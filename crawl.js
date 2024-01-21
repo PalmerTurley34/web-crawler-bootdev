69e177bd2859907f90c861abf866447cb1cc65f9
Palmer-biofi <97751301+Palmer-biofi@users.noreply.github.com>
2024-01-20 23:17:08 -0700

    add url exratct function

--- crawl.js
+++ crawl.js
@@ -1,11 +1,26 @@
+const {JSDOM} = require('jsdom')
+
 function normalizeURL(url) {
     const urlObject = new URL(url)
     const pathname = urlObject.pathname.at(-1) === '/' ? urlObject.pathname : `${urlObject.pathname}/`
     return `${urlObject.hostname}/${pathname}`
 }
 
-const url = new URL('http://example.com/path/')
+function getURLsFromHTML(htmlDoc, baseURL) {
+    const dom = new JSDOM(htmlDoc)
+    const a_tags = dom.window.document.querySelectorAll('a')
+    const urls = []
+    for (tag of a_tags) {
+        if (tag.href[0] === '/') {
+            urls.push(`${baseURL}${tag.href}`)
+        } else {
+            urls.push(tag.href)
+        }
+    }
+    return urls
+}
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
--- crawl.test.js
+++ crawl.test.js
@@ -1,29 +1,36 @@
 const {test, expect} = require('@jest/globals')
-const {normalizeURL} = require('./crawl')
+const {JSDOM} = require('jsdom')
+const {normalizeURL, getURLsFromHTML} = require('./crawl')
 
-test('test http and https are the same', () => {
-    const https_url = 'https://example.com'
-    const http_url = 'http://example.com'
-    expect(normalizeURL(https_url)).toBe(normalizeURL(http_url))
-})
-
-test('test ending / is removed', () => {
-    const with_slash = 'http://example.com/some/path/'
-    const without_slash = 'http://example.com/some/path'
-    expect(normalizeURL(with_slash)).toBe(normalizeURL(without_slash))
-})
-
-test('test query parameters are removed', () => {
-    const with_query = 'http://example.com/path?query=something&parameter=else'
-    const without_query = 'http://example.com/path'
-    expect(normalizeURL(with_query)).toBe(normalizeURL(without_query))
-})
-
-test('combination of other tests', () => {
+test('test normilizeURL', () => {
+    const https_url = normalizeURL('https://example.com')
+    const http_url = normalizeURL('http://example.com')
+    const with_slash = normalizeURL('http://example.com/some/path/')
+    const without_slash = normalizeURL('http://example.com/some/path')
+    const with_query = normalizeURL('http://example.com/path?query=something&parameter=else')
+    const without_query = normalizeURL('http://example.com/path')
     const test1 = normalizeURL('https://example.com/path/?something=true')
     const test2 = normalizeURL('http://example.com/path?more=queries&second=query')
     const test3 = normalizeURL('http://example.com/path')
-
+    expect(https_url).toBe(http_url)
+    expect(with_slash).toBe(without_slash)
+    expect(with_query).toBe(without_query)
     expect(test1).toBe(test2)
     expect(test2).toBe(test3)    
+})
+
+test('test getURLsFromHTML', () => {
+    const testhtml1 = `<html><body><a href="http://example.com"></a></body></html>`
+    const testhtml2 = `<html><body><a href="/test"></a></body></html>`
+    const testhtml3 = `<html><body><a href="/test"></a><a href="/test/24"></a></body></html>`
+    const testBase = 'http://example.com'
+    const test1 = getURLsFromHTML(testhtml1, testBase)
+    const test2 = getURLsFromHTML(testhtml2, testBase)
+    const test3 = getURLsFromHTML(testhtml3, testBase)
+    const expected1 = ['http://example.com/']
+    const expected2 = ['http://example.com/test']
+    const expected3 = ['http://example.com/test', 'http://example.com/test/24']
+    expect(test1).toStrictEqual(expected1)
+    expect(test2).toStrictEqual(expected2)
+    expect(test3).toStrictEqual(expected3)
 })
